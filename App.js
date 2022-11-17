5e245c7d1ba33b0c9641039c8346c6a360ddad0e
TheCodister <quang.nguyen26012907@hcmut.edu.vn>
2022-11-17 21:39:59 +0700

    Added new files

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./App.css";
+
+const App = () => {
+    return (
+    <div className="body">
+        <div className = 'title'>
+            <style>
+                @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap');
+            </style>
+            <h1>Free Heart</h1>
+                <div className='comment'>
+                    <style>
+                        @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap');
+                    </style>
+                    <p>Only you deserve this my luv</p>
+                </div>
+        </div>
+            <div className='heart'></div>
+    </div> 
+    );
+}
+
+export default App;
